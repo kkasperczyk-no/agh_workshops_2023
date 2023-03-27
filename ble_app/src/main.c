d66dc91a2ffb2df8a5a3e6513789e7ede3845ed5
Kamil Kasperczyk <kamil.kasperczyk@nordicsemi.no>
2023-03-27 11:25:15 +0200

    Task 11.2 - Add BLE NUS support

--- ble_app/src/main.c
+++ ble_app/src/main.c
@@ -8,6 +8,8 @@
 #include <zephyr/logging/log.h>
 
 #include <zephyr/bluetooth/bluetooth.h>
+#include <zephyr/bluetooth/conn.h>
+#include <bluetooth/services/nus.h>
 
 LOG_MODULE_REGISTER(APP);
 
@@ -16,13 +18,38 @@ static const struct bt_data advertising_data[] = {
 	BT_DATA(BT_DATA_NAME_COMPLETE, CONFIG_BT_DEVICE_NAME, sizeof(CONFIG_BT_DEVICE_NAME))
 };
 
+static const struct bt_data service_data[] = {
+	BT_DATA_BYTES(BT_DATA_UUID128_ALL, BT_UUID_NUS_VAL),
+};
+
+static void bluetooth_connected(struct bt_conn* conn, uint8_t err) {
+	LOG_INF("Bluetooth LE connected");
+}
+
+static void bluetooth_disconnected(struct bt_conn* conn, uint8_t reason) {
+	LOG_INF("Bluetooth LE disconnected");
+}
+
+static void nus_received(struct bt_conn* conn, const uint8_t* const data, uint16_t len) {
+	LOG_INF("Bluetooth LE NUS received data: %s", data);
+}
+
+static struct bt_conn_cb bluetooth_callbacks = {
+	.connected = bluetooth_connected,
+	.disconnected = bluetooth_disconnected,
+};
+
+static struct bt_nus_cb nus_callbacks = {
+	.received = nus_received,
+};
+
 static void bluetooth_init(int err) {
 	if (err) {
 		LOG_ERR("Bluetooth init failed (err %d)", err);
 		return;
 	}
 
-	err = bt_le_adv_start(BT_LE_ADV_CONN, advertising_data, ARRAY_SIZE(advertising_data), NULL, 0);
+	err = bt_le_adv_start(BT_LE_ADV_CONN, advertising_data, ARRAY_SIZE(advertising_data), service_data, ARRAY_SIZE(service_data));
 	if (err) {
 		LOG_ERR("Bluetooth LE advertising start failed (err %d)", err);
 	} else {
@@ -42,4 +69,12 @@ void main(void)
 		LOG_INF("Bluetooth LE initialization succeeded");
 	}
 
+	bt_conn_cb_register(&bluetooth_callbacks);
+
+	err = bt_nus_init(&nus_callbacks);
+	if (err) {
+		LOG_ERR("NUS initialization failed!");
+	} else {
+		LOG_INF("NUS initialization succeeded");
+	}
 }
